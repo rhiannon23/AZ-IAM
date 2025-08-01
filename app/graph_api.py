9095e0b88de4189b8f10b51f342d4471a525391d
Rhiannon Barbero <rhibarbero@Rhiannons-MacBook-Air.local>
2025-08-01 02:33:41 -0400

    small

---
+++ app/graph_api.py
@@ -0,0 +1,7 @@
+def get_users():
+    # Sample data until fetch from graph
+    return [
+        {"name": "Alice", "lastSignIn": 120, "admin": True},
+        {"name": "Bob", "lastSignIn": 20, "admin": False},
+        {"name": "Charlie", "lastSignIn": 200, "admin": True}
+    ]
